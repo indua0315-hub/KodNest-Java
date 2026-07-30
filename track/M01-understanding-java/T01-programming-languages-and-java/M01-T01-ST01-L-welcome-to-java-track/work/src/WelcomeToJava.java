7ff15872637f181bb7a20fd7d6ef008007711799
indu <indua0315@gmail.com>
2026-07-30 12:48:22 +0530

    day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the  Java track");
+        System.out.println("We are excited to have you with us");
+        System.out.println("Now i am learning how to express that logic using java ");
     }
 }
