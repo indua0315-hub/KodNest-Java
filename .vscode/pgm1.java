9fc4e55f6bf2659fddbb2d5e6b95612e36142530
indu <indua0315@gmail.com>
2026-08-17 12:49:13 +0530

    ArrayPR1

---
+++ .vscode/pgm1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class pgm1 {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int a[] = new int[5];
+            System.out.println("Enter Array Elements :");
+            for (int i = 0; i <= a.length - 1; i++) {
+                a[i] = scan.nextInt();
+            }
+            System.out.println(" Array Elements are :");
+            for (int i = 0; i <= a.length - 1; i++) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
